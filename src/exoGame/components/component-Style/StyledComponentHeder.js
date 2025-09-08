726b00fb031cc73ff03c70dbf68811c1eec75140
erfan <erfangkl1@gmail.com>
2025-09-08 14:02:11 +0200

    responsiveDesignforHeder

--- src/exoGame/components/component-Style/StyledComponentHeder.js
+++ src/exoGame/components/component-Style/StyledComponentHeder.js
@@ -1,31 +1,36 @@
-import Tooltip from "@mui/material/Tooltip";
 import { AppBar, TextField, Toolbar, Box } from "@mui/material";
 import { styled } from "@mui/material/styles";
+import { margin, width } from "@mui/system";
 import { Link } from "react-router-dom";
 
-const StyledAppBar = styled(AppBar)({
-  backgroundColor: "#ffff",
+const StyledAppBar = styled(AppBar)(({ theme }) => ({
   direction: "rtl",
-});
-
+  height: "125px",
+  backgroundColor: "#ffff",
+}));
 const CoustomToolbar = styled(Toolbar)({
   display: "flex",
   justifyContent: "space-between",
   alignItems: "center",
-  height: "60px",
+  height: "200px",
   overflowY: "hidden",
 });
 
-const SerchField = styled(TextField)({
-  width: "50%",
+const SerchField = styled(TextField)(({ theme }) => ({
+  width: "75%",
+  height: "35%",
+  overflow: "hidden",
   direction: "rtl",
   backgroundColor: "#e6e6e6",
   borderRadius: "6px",
-  marginLeft: "250px",
   fontFamily: "vazir",
-});
+  position: "relative",
+  boxShadow:
+    " rgba(0, 0, 0, 0.25) 0px 3px 15px, rgba(0, 0, 0, 0.22) 0px 3px 10px",
+  [theme.breakpoints.down("sm")]: { width: "100%" },
+}));
 
-const BoxIcons = styled(Box)({
+const BoxIcons = styled(Box)(({ theme }) => ({
   color: "#858585",
   cursor: "pointer",
   display: "flex",
@@ -33,21 +38,18 @@ const BoxIcons = styled(Box)({
   alignItems: "center",
   gap: "10px",
   height: "100px",
-  width: "150px",
-});
+  width: "100px",
+  [theme.breakpoints.down("sm")]: { position: "absolute", bottom: "35%" },
+}));
 
-const Spans = styled(Box)({
-  margin: "16px",
+const Spans = styled(Box)(({ theme }) => ({
+  margin: "7px",
   cursor: "pointer",
   fontSize: "13px",
   color: "#606060",
-});
-
-const LogoImg = styled(Box)({
-  width: "70px",
-  height: "70px",
-  cursor: "pointer",
-});
+  width: "50px",
+  flex: "1",
+}));
 
 const Icons = styled(Box)({
   transition: "all 0.3s ease-in-out",
@@ -57,20 +59,64 @@ const Icons = styled(Box)({
   },
 });
 
-const TooltipBox = styled(Tooltip)({
+const TooltipBox = styled(Box)(({ theme }) => ({
   display: "flex",
-  textAlign: "right",
-  gap: "30px",
-  position: "relative",
+  textAlign: "left",
+  justifyContent: "space-around",
   width: "100%",
-  height: "10%",
-});
+  height: "125%",
+  [theme.breakpoints.down("sm")]: { display: "none" },
+}));
 
 const LinkRoter = styled(Link)({
   textDecoration: "none",
   color: "#606060",
-  width: "30px",
 });
+const LogoImg = styled(Box)(({ theme }) => ({
+  width: "70px",
+  height: "70px",
+  cursor: "pointer",
+  [theme.breakpoints.down("sm")]: { width: "50px", height: "50px" },
+}));
+const DrawerDisplay = styled(Box)(({ theme }) => ({
+  [theme.breakpoints.down("xs")]: { display: "none" },
+  [theme.breakpoints.down("xl")]: { display: "none" },
+  [theme.breakpoints.down("lg")]: { display: "none" },
+  [theme.breakpoints.down("md")]: { display: "none" },
+  [theme.breakpoints.down("sm")]: { display: "block" },
+}));
+const SmallSizeBoxLogoAndInput = styled(Box)(({ theme }) => ({
+  [theme.breakpoints.down("xl")]: {
+    display: "none",
+  },
+  [theme.breakpoints.down("xs")]: {
+    display: "none",
+  },
+
+  [theme.breakpoints.down("lg")]: { display: "none" },
+  [theme.breakpoints.down("md")]: { display: "none" },
+  [theme.breakpoints.down("sm")]: {
+    margin: "0 auto",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "75%",
+    height: "100%",
+    padding: "2px",
+  },
+}));
+const MediumeAndLargeBoxLogoAndInput = styled(Box)(({ theme }) => ({
+  display: "flex",
+  gap: "30px",
+  justifyContent: "space-between",
+  alignItems: "center",
+  width: "80%",
+  padding: "1%",
+  [theme.breakpoints.down("sm")]: {
+    display: "none",
+  },
+}));
 
 const styleComponents = {
   LogoImg,
@@ -82,6 +128,9 @@ const styleComponents = {
   Icons,
   TooltipBox,
   LinkRoter,
+  DrawerDisplay,
+  SmallSizeBoxLogoAndInput,
+  MediumeAndLargeBoxLogoAndInput,
 };
 
 export default styleComponents;
