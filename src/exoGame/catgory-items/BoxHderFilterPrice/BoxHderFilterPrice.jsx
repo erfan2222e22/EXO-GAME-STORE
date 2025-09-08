403cf6df056565e63bc9687e4a701a84423a007a
programer <erfangkl1@Gmail.com>
2025-09-08 14:17:35 +0200

    Update BoxHderFilterPrice.jsx

--- src/exoGame/catgory-items/BoxHderFilterPrice/BoxHderFilterPrice.jsx
+++ src/exoGame/catgory-items/BoxHderFilterPrice/BoxHderFilterPrice.jsx
@@ -1,56 +1,53 @@
-import styleComponents from "../../components/component-Style/StyleCatgory";
+import styleComponents from "../../components/component-Style/StyleHederFilterPrice";
 import { Typography } from "@mui/material";
 import { useState } from "react";
-const BoxHederFilterPrice = ({ originalItems, setFilteredItems }) => {
-  const { BoxHderFilterPrice, SortFilterText } = styleComponents;
-  const [valid, setValid] = useState(false);
-  const [valid2, setValid2] = useState(false);
+import AddchartIcon from "@mui/icons-material/Addchart";
+const BoxHederFilterPrice = ({ originalItems, setFilteredItems, setValid }) => {
+  const { BoxHderFilterPrice, SortFilterText, BtnDisplayCatgory } =
+    styleComponents;
+  const [switchToMaxPrice, setSwitchToMaxPrice] = useState(false);
+  const [switchToLowerPrice, setSwitchToLowerPrice] = useState(false);
   const heightPriceFilterItems = () => {
     const highe = originalItems.sort((a, b) => b.price - a.price);
     setFilteredItems((prev) => (prev = highe));
-    setValid(true);
-    if (valid2) {
-      setValid2(false);
+    setSwitchToMaxPrice(true);
+    if (switchToLowerPrice) {
+      setSwitchToLowerPrice(false);
       setFilteredItems([]);
     }
   };
+
   const lowerPriceFilterItems = () => {
     const low = originalItems.sort((a, b) => a.price - b.price);
     setFilteredItems((prev) => (prev = low));
-    setValid2(true);
-    if (valid) {
-      setValid(false);
+    setSwitchToLowerPrice(true);
+    if (switchToMaxPrice) {
+      setSwitchToMaxPrice(false);
       setFilteredItems([]);
     }
   };
 
-  const defualtProductSorted = () => {
-    // setFilterValue((preve) => ({
-    //   ...preve,
-    //   company: "",
-    //   genration: "",
-    //   maxPrice: 0,
-    //   minPrice: 0,
-    // }));
-    // setOriginalItems(initialItems);
-    // setFilteredItems([]);
-    // console.log(originalItems);
-  };
+  const defualtProductSorted = () => {};
 
   return (
     <BoxHderFilterPrice>
+      <BtnDisplayCatgory onClick={() => setValid((prev) => (prev = true))}>
+        <AddchartIcon></AddchartIcon>
+      </BtnDisplayCatgory>
+
       <SortFilterText onClick={heightPriceFilterItems}>
-        بیشترین قیمت
+        Highest price
       </SortFilterText>
       <SortFilterText onClick={lowerPriceFilterItems}>
-        کمترین قیمت
+        Lowest price
       </SortFilterText>
-      <SortFilterText onClick={defualtProductSorted}>پیش فرض</SortFilterText>
+      <SortFilterText onClick={defualtProductSorted}>Default</SortFilterText>
       <Typography sx={{ fontFamily: "vazir", overflow: "hidden" }}>
-        :مرتب کردن براساس
+        Sort by:
       </Typography>
     </BoxHderFilterPrice>
   );
 };
 
 export default BoxHederFilterPrice;
+
