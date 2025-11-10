b1ac474e199bc45132c51005077e1bc0d233ae9b
erfan <erfangkl1@gmail.com>
2025-11-10 16:42:08 +0100

    add404UndifindPage

---
+++ src/exoGame/404UndefindPage/404PageErroe.jsx
@@ -0,0 +1,12 @@
+import Card from "./cardBox";
+import styleComponents from "./styleComponent/style404PageErroe";
+const Page404Erroe = () => {
+  const { ParentBox } = styleComponents;
+  return (
+    <ParentBox>
+      <Card />
+    </ParentBox>
+  );
+};
+
+export default Page404Erroe;
