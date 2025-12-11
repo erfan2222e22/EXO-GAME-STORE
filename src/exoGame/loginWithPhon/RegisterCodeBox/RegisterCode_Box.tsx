5e8058135d0766eeb12e6a1d2add2d0652434b9b
erfan <erfangkl1@gmail.com>
2025-12-11 20:21:33 +0100

    add some Capability on register-Code

--- src/exoGame/loginWithPhon/RegisterCodeBox/RegisterCode_Box.tsx
+++ src/exoGame/loginWithPhon/RegisterCodeBox/RegisterCode_Box.tsx
@@ -1,45 +1,69 @@
-import React, { useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import styleComponent from "./style_Component/Style-RegisterCode-Box";
-import { Box } from "@mui/system";
 import { useLocation } from "react-router-dom";
 import { Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const RegisterCodeBox = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const { code } = location.state;
-  const { Input, ContinuBtn, BoxConteriner, ParentBox, InputParentBox } =
-    styleComponent;
-  const [UserInputCode, setUserInputCode] = useState(
-    Array.from({ length: code.length }, (_, index) => [""])
+  const {
+    Input,
+    ContinuBtn,
+    BoxConteriner,
+    ParentBox,
+    InputParentBox,
+    HederImg,
+  } = styleComponent;
+  const [UserInputCode, setUserInputCode] = useState<string[]>(
+    Array.from({ length: code.length }, () => "")
   );
-
+  const inputRefs = useRef<Array<HTMLInputElement | null>>([]);
   const [validInterdCode, setValidInterdCode] = useState(null);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setValidInterdCode(null);
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, [validInterdCode]);
+
   const handelOnChange = (value: string, index: number) => {
-    setUserInputCode((prev: string[][]) =>
-      prev.map((item: any, index2: number) => {
-        return index2 === index && isNaN(+value) === false ? +[value] : item;
-      })
+    if (isNaN(+value)) return;
+
+    setUserInputCode((prev) =>
+      prev.map((item, index2) => (index2 === index ? value : item))
     );
+
+    if (value.length === 1 && inputRefs.current[index + 1]) {
+      inputRefs.current[index + 1]?.focus();
+    }
   };
 
   const handelOnClick = () => {
     const ReciveCoded = code.flatMap((item: number[]) => item);
 
-    const TestCode = UserInputCode.every(
-      (item, index) => Number(item) === ReciveCoded[index]
-    );
+    const TestCode = UserInputCode.every((item, index) => {
+      return Number(item) === ReciveCoded[index];
+    });
     TestCode
       ? setValidInterdCode((prev: boolean) => (prev = true))
       : setValidInterdCode((prev: boolean) => (prev = false));
 
     setUserInputCode((prev) =>
       prev.map((item) => {
-        return item.toString().trim().length > 0 ? [""] : item;
+        return item.toString().trim().length > 0 ? "" : item;
       })
     );
 
-    console.log(ReciveCoded);
+    validInterdCode && Navigation();
+  };
+
+  const Navigation = () => {
+    navigate(`/register-user`, {
+      state: { products: "" },
+    });
   };
 
   const SetColorElment = (): string => {
@@ -61,22 +85,29 @@ const RegisterCodeBox = () => {
   return (
     <BoxConteriner>
       <ParentBox>
+        <HederImg
+          as="img"
+          aria-label="."
+          src="https://exo.ir/catalog/view/theme/exo/image/logo.svg"
+        ></HederImg>
         <InputParentBox>
-          {UserInputCode.map((item: string[] | number[], index: number) => {
+          {UserInputCode.map((item: string, index: number) => {
             return (
-              <>
-                <Input
-                  onChange={(e) => handelOnChange(e.target.value, index)}
-                  aria-label="."
-                  type="tel"
-                  value={item}
-                  inputMode="numeric"
-                  inputProps={{ maxLength: 1 }}
-                  sx={{
-                    borderColor: SetColorElment(),
-                  }}
-                ></Input>
-              </>
+              <Input
+                onChange={(e) => handelOnChange(e.target.value, index)}
+                aria-label="."
+                value={item}
+                type="tel"
+                inputMode="numeric"
+                inputProps={{
+                  maxLength: 1,
+                  style: { textAlign: "center" },
+                }}
+                sx={{ borderColor: SetColorElment() }}
+                inputRef={(el: HTMLInputElement | null) => {
+                  inputRefs.current[index] = el;
+                }}
+              ></Input>
             );
           })}
         </InputParentBox>
