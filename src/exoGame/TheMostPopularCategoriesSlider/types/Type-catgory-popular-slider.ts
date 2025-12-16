869a81fd0c737177d113037f71dd666f865c1c80
erfan <erfangkl1@gmail.com>
2025-12-16 13:46:25 +0100

    add types on Catgory-Popular-Slider

---
+++ src/exoGame/TheMostPopularCategoriesSlider/types/Type-catgory-popular-slider.ts
@@ -0,0 +1,8 @@
+ interface Type_ProductList_enteris {
+  src: string;
+  text: string;
+  title: string;
+  id: number;
+  jsonServer: string;
+}
+export type Type_handelOnClick = (items: Type_ProductList_enteris) => void;
