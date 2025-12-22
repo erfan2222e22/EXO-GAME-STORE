90d1618d38b807010ffd320553cd267cd1f2e7f8
erfan <erfangkl1@gmail.com>
2025-12-22 21:51:53 +0100

    refactor filterSlider

--- src/exoGame/catgory-items/filterSlider.tsx
+++ src/exoGame/catgory-items/filterSlider.tsx
@@ -1,6 +1,6 @@
 import { Typography, Box, Slider } from "@mui/material";
 import component from "./Style-Component/StyleFilterSlider";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Props_Component, Type_handelOnchange } from "./Types_FilterSlider";
 const FilterSlider: Props_Component = ({ setPriceFunction }) => {
   const { InsideBox, TextItems } = component;
@@ -10,20 +10,6 @@ const FilterSlider: Props_Component = ({ setPriceFunction }) => {
     minValuePrice,
     maxValuePrice,
   ]);
-  const [ItemsSlider, ItemsSetSlider] = useState([]);
-
-  useEffect(() => {
-    const allValidItems = value_filters.map((item) => item.valueFillter);
-    ItemsSetSlider(allValidItems);
-  }, []);
-
-  let value_filters = [
-    {
-      id: 1,
-      valueFillter: [1],
-    },
-  ];
-
   const handelOnchange: Type_handelOnchange = (event, newitem) => {
     setValueSlider(newitem);
     setPriceFunction({
@@ -33,44 +19,36 @@ const FilterSlider: Props_Component = ({ setPriceFunction }) => {
   };
 
   return (
-    <>
-      {ItemsSlider.map((item) => {
-        return (
-          <>
-            <InsideBox key={item.id}>
-              <Slider
-                valueLabelDisplay="auto"
-                onChange={handelOnchange}
-                value={valueSlider}
-                max={maxValuePrice}
-                min={minValuePrice}
-              />
-              <Box
-                sx={{
-                  display: "flex",
-                  textAlign: "center",
-                }}
-              >
-                <Box>
-                  <Typography>{valueSlider[0]}</Typography>
-                  <TextItems>$</TextItems>
-                </Box>
-                <Box
-                  sx={{
-                    margin: "0 auto",
-                    position: "relative",
-                    left: "30%",
-                  }}
-                >
-                  <Typography>{valueSlider[1]}</Typography>
-                  <TextItems>$</TextItems>
-                </Box>
-              </Box>
-            </InsideBox>
-          </>
-        );
-      })}
-    </>
+    <InsideBox>
+      <Slider
+        valueLabelDisplay="auto"
+        onChange={handelOnchange}
+        value={valueSlider}
+        max={maxValuePrice}
+        min={minValuePrice}
+      />
+      <Box
+        sx={{
+          display: "flex",
+          textAlign: "center",
+        }}
+      >
+        <Box>
+          <Typography>{valueSlider[0]}</Typography>
+          <TextItems>$</TextItems>
+        </Box>
+        <Box
+          sx={{
+            margin: "0 auto",
+            position: "relative",
+            left: "30%",
+          }}
+        >
+          <Typography>{valueSlider[1]}</Typography>
+          <TextItems>$</TextItems>
+        </Box>
+      </Box>
+    </InsideBox>
   );
 };
 
