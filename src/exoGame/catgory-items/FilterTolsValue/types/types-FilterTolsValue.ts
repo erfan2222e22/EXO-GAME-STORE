d80c261f65883d081f68b5a4c86d5a6bf84db7bc
erfan <erfangkl1@gmail.com>
2025-12-22 21:59:48 +0100

    add types on FilterTool Values

---
+++ src/exoGame/catgory-items/FilterTolsValue/types/types-FilterTolsValue.ts
@@ -0,0 +1,20 @@
+import {
+  Type_FilterValuse,
+  FilterConfigItem,
+  setPriceFunction,
+} from "../../mainCatgoryPage/types/types-Catgory";
+import { Item_Setting_Interface } from "../../mainCatgoryPage/types/Type_CatgoryJsx";
+
+export interface Component_Props_Interface {
+  props: {
+    itemsSetting: Item_Setting_Interface;
+    setPriceFunction: setPriceFunction;
+    setFilterValue: React.Dispatch<React.SetStateAction<Type_FilterValuse>>;
+    kind_filters: FilterConfigItem[];
+    FilterValue: Type_FilterValuse;
+  };
+}
+
+export type Type_ComponentProps = ({
+  ...props
+}: Component_Props_Interface) => React.ReactNode;
