5cdeb208c52a36876ae31d2bda34595860a6d193
erfan <erfangkl1@gmail.com>
2025-12-30 18:34:29 +0100

    add orderHistoryAccount on userAccount

--- src/exoGame/userAcount/orderHistoryAccount/style-Component/style-OrderHistoryAccount.js
+++ src/exoGame/userAcount/orderHistoryAccount/style-Component/style-OrderHistoryAccount.js
@@ -1,59 +1,37 @@
 import { Box } from "@mui/material";
 import { styled } from "@mui/material/styles";
-
 const Div = styled(Box)({
-  display: "flex",
-  flexDirection: "column",
-  width: "100%",
-  justifyContent: "center",
-  gap: "10px",
+  width: "80%",
+  padding: "5px",
 });
 
-const ParentDiv = styled(Box)({
-  padding: "1%",
-  display: "flex",
-  flexDirection: "column",
-  gap: "1px",
-  width: "90%",
-  borderRadius: "5px",
-  boxShadow: " rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
-  marginLeft: "20px",
-});
 const HederParntDiv = styled(Box)({
+  padding: "5px",
+  borderRadius: "1.5%",
   overflow: "hidden",
   display: "flex",
   justifyContent: "space-between",
   backgroundColor: "#d4f7df",
   alignItems: "center",
   width: "100%",
-  height: "30%",
+  height: "10%",
   boxShadow:
     " rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px",
 });
 const HderContinerDiv = styled(Box)({
   display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  gap: "10%",
-  width: "35%",
+  gap: "5%",
+  width: "100%",
   textAlign: "center",
 });
 const BoxTitle = styled(Box)({
   fontSize: "1.1rem",
   color: "#3c3c3c",
 });
-
-const ParentDivText = styled(Box)({
-  marginLeft: "5px",
-  overflow: "hidden",
-});
-
 const styleComponent = {
-  ParentDiv,
   HederParntDiv,
   HderContinerDiv,
   BoxTitle,
-  ParentDivText,
   Div,
 };
 
