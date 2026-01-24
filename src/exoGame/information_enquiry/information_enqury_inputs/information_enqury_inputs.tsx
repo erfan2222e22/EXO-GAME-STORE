b58196b98b856ed5f8f82ee5753ba20c6386e717
erfan <erfangkl1@gmail.com>
2026-01-24 13:09:22 +0100

    refactor information enquiry

--- src/exoGame/information_enquiry/information_enqury_inputs/information_enqury_inputs.tsx
+++ src/exoGame/information_enquiry/information_enqury_inputs/information_enqury_inputs.tsx
@@ -1,14 +1,18 @@
-import { useContext } from "react";
+import React, { useContext } from "react";
 import contextUse from "../../useContext/useContext";
-import { Box } from "@mui/system";
-import { Typography } from "@mui/material";
-import Autocomplete from "@mui/material/Autocomplete";
-import TextField from "@mui/material/TextField";
+import { Typography, Box } from "@mui/material";
 import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined";
 import styleComponent from "./Style-Component/StyleInformationEnqury";
-import { Types_useContext } from "./types/Types_information_enqury_inputs";
+import ElmentsMapedJsx from "./elmentsMapedJsx/elmentsMapedJsx";
+import {
+  Types_useContext,
+  Type_HandelOnChange,
+  Type_handelAutocomplete,
+  Type_PhoneNumberHndelErroe,
+} from "./types/Types_information_enqury_inputs";
+
 const InformationEnquryInputs = () => {
-  const { Inputs, ContinerInputs, HederBoxContiner } = styleComponent;
+  const { ContinerInputs, HederBoxContiner } = styleComponent;
   const {
     elmentsInArray,
     failRequest,
@@ -19,9 +23,12 @@ const InformationEnquryInputs = () => {
     setPhoneNumberEroreBolean,
     setPhoneNumberEroreText,
   }: Types_useContext = useContext(contextUse);
-
-  const PhoneNumberHndelErroe = (ItemName: string, e: any) => {
-    const valueNumber = e.replace(/\D/g, "");
+  // ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  const PhoneNumberHndelErroe: Type_PhoneNumberHndelErroe = (
+    ItemName,
+    valueText
+  ) => {
+    const valueNumber = valueText.replace(/\D/g, "");
 
     if (ItemName === "phoneNumber") {
       if (valueNumber.length >= 1 && valueNumber[0] !== "0") {
@@ -40,43 +47,36 @@ const InformationEnquryInputs = () => {
     }
   };
 
-  const handelAutocomplete = (e: any, newValue: any) => {
+  const handelAutocomplete: Type_handelAutocomplete = (e) => {
     setElmentInArray((prev) =>
       prev.map((item) =>
         item.name === "howNotifYou"
-          ? { ...item, value: newValue?.label || {} }
+          ? { ...item, value: e.currentTarget.innerText ?? "" }
           : item
       )
     );
   };
 
-  const handelOnchange = (itemName: string, e: any) => {
-    let valueText = e.target.value;
+  const handelOnchange: Type_HandelOnChange = (itemName, e) => {
+    let valueInput = e.target.value;
 
     setRequest((prev) => ({
       ...prev,
-      [itemName]: e,
+      [itemName]: valueInput,
     }));
 
     if (itemName === "phoneNumber") {
-      valueText = e.replace(/\D/g, "");
-      PhoneNumberHndelErroe(itemName, e);
+      valueInput = valueInput.replace(/\D/g, "");
+      PhoneNumberHndelErroe(itemName, valueInput);
     }
 
     setElmentInArray((prev) =>
       prev.map((item) =>
-        item.name === itemName ? { ...item, value: valueText } : item
+        item.name === itemName ? { ...item, value: valueInput } : item
       )
     );
+    console.log(elmentsInArray);
   };
-
-  const AutocompleteOptions = [
-    { label: "pleas chose" },
-    { label: "telegram" },
-    { label: "whatsap" },
-    { label: "call" },
-    { label: "email" },
-  ];
 
   return (
     <Box>
@@ -89,80 +89,14 @@ const InformationEnquryInputs = () => {
         </Typography>
       </HederBoxContiner>
       <ContinerInputs>
-        {elmentsInArray.map((item) => {
-          return (
-            <Box zIndex={item.id}>
-              <Box>
-                <Box
-                  sx={{
-                    display: "flex",
-                    textAlign: "center",
-                    gap: "5px",
-                  }}
-                >
-                  <Typography>{item.hederText}</Typography>
-                  {item.importeData && (
-                    <Typography sx={{ color: "red" }}>*</Typography>
-                  )}
-                </Box>
-                <Box
-                  sx={{
-                    margin: "10px",
-                  }}
-                >
-                  {item.type === "input" && (
-                    <Inputs
-                      as="input"
-                      type={item.name === "phoneNumber" ? "tel" : "text"}
-                      onChange={(e) => handelOnchange(item.name, e)}
-                      value={item.value}
-                      placeholder={
-                        item.name === "phoneNumber"
-                          ? "frist num 0 and num 9"
-                          : ""
-                      }
-                    ></Inputs>
-                  )}
-                  {item.type === "Autocomplete" && (
-                    <Autocomplete
-                      disablePortal
-                      options={AutocompleteOptions}
-                      renderInput={(params) => <TextField {...params} />}
-                      onChange={(e) => handelAutocomplete(e, item)}
-                    ></Autocomplete>
-                  )}
-
-                  {item.importeData &&
-                    item.value.length === 0 &&
-                    failRequest && (
-                      <span style={{ color: "red" }}>
-                        {item.name} need this{" "}
-                      </span>
-                    )}
-
-                  {item.name === "phoneNumber" &&
-                    failRequest &&
-                    phoneNumberEroreBolean && (
-                      <span style={{ color: "red" }}>
-                        {phoneNumberEroreText}
-                      </span>
-                    )}
-
-                  {item.type === "textarea" && (
-                    <textarea
-                      aria-label="."
-                      onChange={(e) =>
-                        handelOnchange(item.name, e.target.value)
-                      }
-                      value={item.value}
-                      style={{ width: "100%" }}
-                    ></textarea>
-                  )}
-                </Box>
-              </Box>
-            </Box>
-          );
-        })}
+        <ElmentsMapedJsx
+          elmentsInArray={elmentsInArray}
+          failRequest={failRequest}
+          phoneNumberEroreBolean={phoneNumberEroreBolean}
+          phoneNumberEroreText={phoneNumberEroreText}
+          handelOnchange={handelOnchange}
+          handelAutocomplete={handelAutocomplete}
+        />
       </ContinerInputs>
     </Box>
   );
